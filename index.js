f2bc84ec179900a82e0792413f0bcd49c1a242ee
Arish-Shah <arish0246@gmail.com>
2020-04-21 14:16:29 +0530

    add middleware

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
+const members = require('./Members');
+const logger = require('./middleware/logger');
+
 const app = express();
+
+// Initialize middleware
+app.use(logger);
+
+// Get all members
+app.get('/api/members', (req, res) => res.json(members));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT} 🚀`));
